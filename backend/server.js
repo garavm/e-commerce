0e63d3c506b1a94dd69e195ec07bc6ede9926302
Gaurav Moohapatra <167212491+PrinceYs@users.noreply.github.com>
2025-02-18 22:23:31 +0530

    [Backend]: Inital setup

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+// including env variables
+dotenv.config();
+const { PORT, DB_PASSWORD, DB_USER } = process.env;
+/**********************connection to our DB********************************/
+const dbURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.drcvhxp.mongodb.net/?retryWrites=true&w=majority`;
+// once 
+mongoose.connect(dbURL)
+    .then(function (connection) {
+        console.log("connected to db");
+    }).catch(err => console.log(err))
+
+// with this your creating simple app -> api
+const app = express();
+const UserRouter = require("./router/UserRouter");
+const ProductRouter = require("./router/ProductRouter");
+const AuthRouter = require("./router/AuthRouter");
+const BookingRouter = require("./router/BookingRouter");
+const ReviewRouter = require("./router/ReviewRouter");
+
+const corsConfig = {
+    origin: true,
+    credentials: true,
+};
+// this is allowing all the requests
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig));
+
+// app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", UserRouter);
+app.use("/api/product", ProductRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/booking", BookingRouter);
+app.use("/api/review",ReviewRouter);
+
+/******************handler functions ***************/
+// 404 route not found
+app.use(function cb(req, res) {
+    // console.log("");
+    // response 
+    res.status(404).json({
+        status: "failure",
+        message: " route not found"
+    })
+})
+
+
+app.listen(PORT, function () {
+    console.log(` server is listening to port ${PORT}`);
+})
